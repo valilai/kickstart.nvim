b1d0663747a961156c0e349560433f0b79e2c6b7
valilai <valentin.lairich@gmail.com>
2025-01-02 16:24:09 +0100

    feat: add luarocks support

--- init.lua
+++ init.lua
@@ -7,6 +7,17 @@ require 'keymaps'
 -- [[ Autocommands ]]
 require 'autocmds'
 
+-- Specify specific lua version to use
+local lua_path = '~/.luaver/lua/5.1.5/bin/'
+local luarocks_path = '~/.luaver/luarocks/3.9.2_5.1/bin/'
+
+if not string.find(vim.env.PATH, lua_path) and not string.find(vim.env.PATH, luarocks_path) then
+  vim.env.PATH = lua_path .. ':' .. luarocks_path .. ':' .. vim.env.PATH
+end
+
+-- Specify specific python env
+vim.g.python3_host_prog = '~/src/miniforge3/envs/nvim/bin/python'
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
